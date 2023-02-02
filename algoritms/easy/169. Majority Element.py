3920cf4d5d0651966f3ee9f39154b4318209e3da
ilia <iliaman1993@gmail.com>
2023-02-02 15:47:21 +0300

    majority element solved

---
+++ algoritms/easy/169. Majority Element.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import List
+
+
+def majorityElement(nums: List[int]) -> int:
+    return Counter(nums).most_common()[0][0]
+
+
+#good solution
+# def majorityElement(self, nums: List[int]) -> int:
+#     x = len(nums)
+#     n = len(nums) // 2
+#     while x != 0:
+#         if (nums.count(nums[x - 1]) > n):
+#             return nums[x - 1]
+#         else:
+#             x -= 1
