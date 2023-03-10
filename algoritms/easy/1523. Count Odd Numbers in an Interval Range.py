34feddd010eaf2dabe465deaa688358f6c96124f
pupsik <iliaman1993@gmail.com>
2023-03-10 17:55:44 +0300

    Count odd numbers solved

--- algoritms/easy/1523. Count Odd Numbers in an Interval Range.py
+++ algoritms/easy/1523. Count Odd Numbers in an Interval Range.py
@@ -1,3 +1,6 @@
 def countOdds(low: int, high: int) -> int:
+    return (high - low) // 2 + 1 if (low % 2 == 1 or high % 2 == 1) else (high - low) // 2
 
-    return len([number for number in range(low, high+1) if number % 2 == 1])
+# slow solution
+# def countOdds(low: int, high: int) -> int:
+#     return len([number for number in range(low, high+1) if number % 2 == 1])
