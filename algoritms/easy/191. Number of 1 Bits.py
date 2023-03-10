bff6bdc367b530f8d7240bc69655f3cab3a0de25
pupsik <iliaman1993@gmail.com>
2023-03-10 13:55:40 +0300

    number of 1 bits solved

--- algoritms/easy/191. Number of 1 Bits.py
+++ algoritms/easy/191. Number of 1 Bits.py
@@ -1,7 +1,2 @@
-from collections import Counter
-
 def hammingWeight(n: int) -> int:
-    return Counter(str(n))['1']
-
-
-print(hammingWeight(11111111111111111111111111111101))
+    return bin(n).count('1')
