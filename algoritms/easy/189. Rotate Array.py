2d18e02594250048bf8fa0d254e1a97cbf6b9d54
pupsik <iliaman1993@gmail.com>
2023-03-11 14:01:44 +0300

    189. Rotate Array solved upgrade

--- algoritms/easy/189. Rotate Array.py
+++ algoritms/easy/189. Rotate Array.py
@@ -2,3 +2,17 @@ def rotate(nums: list[int], k: int) -> None:
     k = k if len(nums) > k else k % len(nums)
     for _ in range(k):
         nums.insert(0, nums.pop())
+
+
+# leetcode propystil
+def rotate1(nums: list[int], k: int) -> None:
+    k = k % len(nums)
+    nums.reverse()
+    nums[:k] = reversed(nums[:k])
+    nums[k:] = reversed(nums[k:])
+
+
+# without cycle
+def rotate2(nums: list[int], k: int) -> None:
+    k = k % len(nums)
+    nums = nums[-k:] + nums[:-k]
