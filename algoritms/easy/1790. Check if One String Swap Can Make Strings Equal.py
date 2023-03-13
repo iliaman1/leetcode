d9604b5604514f81176c51e12778c9d041c0fd04
pupsik <iliaman1993@gmail.com>
2023-03-13 18:35:58 +0300

    1790. Check if One String Swap Can Make Strings Equal solved

--- algoritms/easy/1790. Check if One String Swap Can Make Strings Equal.py
+++ algoritms/easy/1790. Check if One String Swap Can Make Strings Equal.py
@@ -1,4 +1,13 @@
 from collections import Counter
 
 def areAlmostEqual(s1: str, s2: str) -> bool:
-    return Counter(s1) == Counter(s2)
+    if Counter(s1) == Counter(s2):
+        count_diff = 0
+        for index in range(len(s1)):
+            if s1[index] != s2[index]:
+                count_diff += 1
+
+        if count_diff < 3:
+            return True
+
+    return False
