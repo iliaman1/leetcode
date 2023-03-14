82a943863b2448d35a304fce178b5b6621a2668a
pupsik <iliaman1993@gmail.com>
2023-03-14 20:32:32 +0300

    876. Middle of the Linked List solved with pointers

--- algoritms/easy/876. Middle of the Linked List.py
+++ algoritms/easy/876. Middle of the Linked List.py
@@ -10,8 +10,7 @@ class ListNode:
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         pointer1 = pointer2 = head
-        while pointer2:
-            pointer2 = pointer2.next.next
+        while pointer2 and pointer2.next:
             pointer1 = pointer1.next
-
+            pointer2 = pointer2.next.next
         return pointer1
