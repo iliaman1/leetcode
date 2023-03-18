f4c0497402309d3982e6486350c4191d82411370
ilia <iliaman1993@gmail.com>
2023-03-18 11:15:28 +0300

    389. Find the Difference solved

---
+++ algoritms/easy/389. Find the Difference.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def findTheDifference(s: str, t: str) -> str:
+    s = Counter(s)
+    t = Counter(t)
+    res = t-s
+
+    return ''.join(res.keys())
+
+
+# good solution
+def findTheDifference1(s: str, t: str) -> str:
+    for i in set(t):
+        if s.count(i) != t.count(i):
+            return i
