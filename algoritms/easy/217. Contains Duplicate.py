395a50547c70a111b44028119abe206aaffbcec1
pupsik <iliaman1993@gmail.com>
2023-03-20 15:10:21 +0300

    217. Contains Duplicate add good solution

--- algoritms/easy/217. Contains Duplicate.py
+++ algoritms/easy/217. Contains Duplicate.py
@@ -4,3 +4,18 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
+    # good solution
+    def containsDuplicate1(self, nums: List[int]) -> bool:
+
+        numsTracker = {}
+
+        for num in nums:
+            if num in numsTracker:
+                numsTracker[num] = numsTracker[num] + 1
+                if numsTracker[num] > 1:
+                    return True
+            else:
+                numsTracker[num] = 1
+
+        return False
