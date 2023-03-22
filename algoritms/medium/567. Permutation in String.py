6f24772ece7f7700bbea28cbfeb899be24030c7e
ilia <iliaman1993@gmail.com>
2023-03-22 14:53:07 +0300

    567. Permutation in String solved

--- algoritms/medium/567. Permutation in String.py
+++ algoritms/medium/567. Permutation in String.py
@@ -1,23 +1,46 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def checkInclusion(s1: str, s2: str) -> bool:
     if len(s1) > len(s2):
         return False
 
-    basket = []
+    basket = Counter(s1)
+
     for right_index, value in enumerate(s2):
-        if value in s1:
-            left = right_index
-            while left < right_index + len(s1) <= len(s2):
-                basket.append(s2[left])
-                left += 1
-            if Counter(basket) == Counter(s1):
-                return True
-            else:
-                basket.clear()
+        if value in basket:
+            basket[value] -= 1
+
+        if right_index >= len(s1) and s2[right_index - len(s1)] in basket:
+            basket[s2[right_index - len(s1)]] += 1
+
+        if all(basket[i] == 0 for i in basket):
+            return True
 
     return False
 
 
-print(checkInclusion('adc', 'dcda'))
+# good solution
+def checkInclusion1(s1: str, s2: str) -> bool:
+    length = len(s1) - 1
+    window = defaultdict(int)
+    counter = Counter(s1)
+
+    for index, value in enumerate(s2):
+        window[value] += 1
+
+        if window == counter:
+            return True
+
+        if index >= length:
+            key = s2[index - length]
+            window[key] -= 1
+
+            if window[key] == 0:
+                del window[key]
+
+    return False
+
+print(checkInclusion1('adc', 'dcda'))
+print(checkInclusion1('ab', 'eidboaoo'))
+print(checkInclusion1('ab', 'eidbaooo'))
