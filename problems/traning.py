3b5fb7a0074806bfea06b2106ffbc1a11345985a
pupsik <iliaman1993@gmail.com>
2023-04-01 14:03:58 +0300

    add merge_two_lists solution

--- problems/traning.py
+++ problems/traning.py
@@ -165,3 +165,22 @@ def myPow(x: float, n: int) -> float:
         return result if n % 2 == 0 else x * result
 
     return pow(x, n) if n > 0 else 1 / pow(x, abs(n))
+
+
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    if list1 and list2:
+        if list1.val <= list2.val:
+            head, list1 = list1, list1.next
+        else:
+            head, list2 = list2, list2.next
+
+        head.next = mergeTwoLists(list1, list2)
+
+        return head
+
+    else:
+        return list1 or list2
+
+
+
+
