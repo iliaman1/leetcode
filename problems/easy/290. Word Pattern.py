6f719677f199f034c35b7d9fa91595351bfa6b7e
ilia <iliaman1993@gmail.com>
2023-05-25 14:44:04 +0300

    290 word pattern 95% beats solution

--- problems/easy/290. Word Pattern.py
+++ problems/easy/290. Word Pattern.py
@@ -6,10 +6,19 @@ Here follow means a full match, such that there is a bijection between a letter 
 
 
 def wordPattern(pattern: str, s: str) -> bool:
-    s = [word[0] for word in s.split()]
+    s = s.split()
+    basket = {}
+
     if len(pattern) != len(s):
         return False
 
-    return True
+    for index, letter in enumerate(pattern):
+        if letter not in basket:
+            basket[letter] = s[index]
+        if basket[letter] != s[index]:
+            return False
 
-wordPattern("abba", "dog cat cat dog")
+    if len(set(basket.keys())) != len(set(basket.values())):
+        return False
+
+    return True
