ac60b0fb272b88a287a538d458d50bf1a5e29f59
iliaman <iliaman1993@gmail.com>
2023-06-10 17:04:12 +0300

    37/38

--- problems/medium/820. Short Encoding of Words.py
+++ problems/medium/820. Short Encoding of Words.py
@@ -4,17 +4,16 @@ from operator import itemgetter
 
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        def encode(words: List[str]) -> str:
-            res = ''
-            index = 0
-            words.sort(key=lambda x: len(x), reverse=True)
-            while index < len(words):
-                if words[index] + '#' not in res:
-                    res += words[index] + '#'
-                index += 1
-            return res
+        basket = {}
+        for index, word in enumerate(words):
+            for index2, word2 in enumerate(words):
+                if index != index2:
+                    if word.endswith(word2) and index2 not in basket:
+                        basket[index2] = word2
 
-        return len(encode(words))
+        for indexes in sorted(basket.keys(), reverse=True):
+            del words[indexes]
+        return sum([len(i) for i in words]) + len(words) if words else len(basket[0]) + 1
 
 
 if __name__ == '__main__':
@@ -23,3 +22,5 @@ if __name__ == '__main__':
     assert solution.minimumLengthEncoding(["t"]) == 2
     assert solution.minimumLengthEncoding(["me", "time"]) == 5
     assert solution.minimumLengthEncoding(["p", "grah", "qwosp"]) == 11
+    assert solution.minimumLengthEncoding(["feipyxx", "e"]) == 10
+    assert solution.minimumLengthEncoding(["time", "time", "time", "time"]) == 5
