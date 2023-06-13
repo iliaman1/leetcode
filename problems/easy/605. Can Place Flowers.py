501bc762fbd915d8c9a1ff8a7f854b3a9df42bcb
ilia <iliaman1993@gmail.com>
2023-06-13 14:55:36 +0300

    solved

--- problems/easy/605. Can Place Flowers.py
+++ problems/easy/605. Can Place Flowers.py
@@ -3,16 +3,17 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        pointer = 0
-        count_max_flowers = 0
-        while pointer < len(flowerbed)-1:
-            if flowerbed[pointer] == 0:
-
-
-
+        fb = [0] + flowerbed + [0]
+        for i in range(1, len(fb) - 1):
+            if fb[i - 1] == 0 and fb[i] == 0 and fb[i + 1] == 0:
+                fb[i] = 1
+                n -= 1
+        return n <= 0
 
 
 if __name__ == '__main__':
     solution = Solution()
+    assert solution.canPlaceFlowers([0], 1) is True
     assert solution.canPlaceFlowers([1, 0, 0, 0, 1], 1) is True
     assert solution.canPlaceFlowers([1, 0, 0, 0, 1], 2) is False
+    assert solution.canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2) is True
