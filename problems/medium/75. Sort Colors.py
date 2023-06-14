8bb9971ec026e23980e8385cfa92a48d0f2f5a98
ilia <iliaman1993@gmail.com>
2023-06-14 14:57:04 +0300

    initial

---
+++ problems/medium/75. Sort Colors.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l_pointer = 0
+        r_pointer = len(nums) - 1
+        current = 0
+        while current < r_pointer:
+            if nums[current] == 0:
+                nums[current], nums[l_pointer] = nums[l_pointer], nums[current]
+                l_pointer += 1
+                current += 1
+            elif nums[current] == 2:
+                nums[current], nums[r_pointer] = nums[r_pointer], nums[current]
+                r_pointer -= 1
+                current += 1
+            else:
+                current += 1
+        print(nums)
+
+
+sol = Solution()
+sol.sortColors([1, 2, 0, 2, 0, 1])
