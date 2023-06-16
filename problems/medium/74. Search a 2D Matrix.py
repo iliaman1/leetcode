915b36173a77dc70a70b524c965d239f39f5b9fe
iliaman <iliaman1993@gmail.com>
2023-06-16 16:02:31 +0300

    solved

--- problems/medium/74. Search a 2D Matrix.py
+++ problems/medium/74. Search a 2D Matrix.py
@@ -3,19 +3,20 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows, column = len(matrix), len(matrix[0])
+        rows, column = len(matrix)-1, len(matrix[0])
 
         row_index = -1
 
         l_r = 0
         while l_r <= rows:
             mid_row = l_r + (rows - l_r) // 2
-            if matrix[mid_row][0] > target:
-                rows = mid_row - 1
-            elif matrix[mid_row][-1] < target:
+            if matrix[mid_row][-1] < target:
                 l_r = mid_row + 1
+            elif matrix[mid_row][0] > target:
+                rows = mid_row - 1
             else:
                 row_index = mid_row
+                break
 
         if row_index == -1:
             return False
@@ -23,16 +24,13 @@ class Solution:
         target_row = matrix[row_index]
         l_c = 0
         while l_c <= column:
-            if matrix[row_index][-1] >= target:
-                mid = left + (right - left) // 2
-                if matrix[i][mid] == target:
-                    return True
-                elif matrix[i][mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+            mid = l_c + (column - l_c) // 2
+            if target_row[mid] == target:
+                return True
+            elif target_row[mid] < target:
+                l_c = mid + 1
             else:
-                continue
+                column = mid - 1
 
         return False
 
@@ -49,3 +47,4 @@ if __name__ == '__main__':
         [10, 11, 16, 20],
         [23, 30, 34, 60]
     ], 13) is False
+    assert solution.searchMatrix([[1]], 2) is False
