5b7860e7a388c2d25b06888d9c2ebec17d742016
iliaman <iliaman1993@gmail.com>
2023-06-17 14:08:56 +0300

    solved

--- problems/easy/1046. Last Stone Weight.py
+++ problems/easy/1046. Last Stone Weight.py
@@ -3,16 +3,18 @@ from typing import List
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones.sort()
-        if len(stones) == 1:
-            return stones[0]
-
-        return stones.pop() - self.lastStoneWeight(stones)
+        while len(stones) > 1:
+            stones.sort()
+            stone_max = stones.pop()
+            stone_pre_max = stones.pop()
+            if stone_max != stone_pre_max:
+                stones.append(stone_max - stone_pre_max)
+        return stones[0] if stones else 0
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.lastStoneWeight([2, 7, 4, 1, 8, 1, 10, 6]))
     assert solution.lastStoneWeight([2, 7, 4, 1, 8, 1, 10, 6]) == 1
     assert solution.lastStoneWeight([1]) == 1
     assert solution.lastStoneWeight([2, 4]) == 2
+    assert solution.lastStoneWeight([2, 2]) == 0
