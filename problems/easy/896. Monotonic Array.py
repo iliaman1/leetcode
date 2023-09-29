a85ccc4e41eb6968df4e4a266f84b44bca3024cc
iliaman <iliaman1993@gmail.com>
2023-09-29 11:50:20 +0300

    sep daily

--- problems/easy/896. Monotonic Array.py
+++ problems/easy/896. Monotonic Array.py
@@ -1,2 +1,29 @@
-def isMonotonic(nums: list[int]) -> bool:
-    return nums == sorted(nums) or nums == sorted(nums, reverse=True)
+from typing import List
+
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        currVal = nums[0]
+        desc = False
+        incr = False
+        for i in nums:
+            if i < currVal:
+                desc = True
+            if i > currVal:
+                incr = True
+            if incr and desc:
+                return False
+            currVal = i
+        return True
+
+        # 1 line
+        # return nums == sorted(nums) or nums == sorted(nums, reverse=True)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    assert solution.isMonotonic([1, 2, 2, 3]) is True, 'test 1 failed'
+    assert solution.isMonotonic([6, 5, 4, 4]) is True, 'test 2 failed'
+    assert solution.isMonotonic([1, 3, 2]) is False, 'test 3 failed'
