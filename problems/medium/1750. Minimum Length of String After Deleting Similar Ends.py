ab2f99fc69e473fccdfcac579f496166ce982d02
iliaman <iliaman1993@gmail.com>
2024-03-05 15:35:14 +0300

    mar

---
+++ problems/medium/1750. Minimum Length of String After Deleting Similar Ends.py
@@ -0,0 +1,9 @@
+def minimum_length(s: str) -> int:
+    l, r = 0, len(s) - 1
+    while l < r and s[l] == s[r]:
+        c = s[l]
+        while l <= r and s[l] == c:
+            l += 1
+        while l < r and s[r] == c:
+            r -= 1
+    return r - l + 1
