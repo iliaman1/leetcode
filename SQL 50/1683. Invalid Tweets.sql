5f218d2fd897d522a0ec3a4b1e2e7787e231e0b4
iliaman <iliaman1993@gmail.com>
2024-03-18 16:50:23 +0300

    added SQL schemas

--- SQL 50/1683. Invalid Tweets.sql
+++ SQL 50/1683. Invalid Tweets.sql
@@ -1,3 +1,9 @@
+-- SQL Schema
+Create table If Not Exists Tweets(tweet_id int, content varchar(50))
+Truncate table Tweets
+insert into Tweets (tweet_id, content) values ('1', 'Vote for Biden')
+insert into Tweets (tweet_id, content) values ('2', 'Let us make America great again!')
+
 -- Write your PostgreSQL query statement below
 select tweet_id
 from Tweets
