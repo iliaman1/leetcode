0575162a69bf31736dc96b3a1abda468a07aa453
iliaman <iliaman1993@gmail.com>
2024-03-21 13:52:39 +0300

    Basic Joins

---
+++ SQL 50/197. Rising Temperature.sql
@@ -0,0 +1,12 @@
+-- SQL Schema
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+
+-- Write your PostgreSQL query statement below
+select w1.id
+from Weather w1, Weather w2
+where w1.recordDate - w2.recordDate = 1 and w1.temperature > w2.temperature;
