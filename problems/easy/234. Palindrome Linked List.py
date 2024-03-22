32a87c7097630251f50ba0c4c88fe32fbaeaa4d5
iliaman <iliaman1993@gmail.com>
2024-03-22 13:51:59 +0300

    linked list, two pointers, stack, recursion

--- problems/easy/234. Palindrome Linked List.py
+++ problems/easy/234. Palindrome Linked List.py
@@ -1,3 +1,11 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         res = []
