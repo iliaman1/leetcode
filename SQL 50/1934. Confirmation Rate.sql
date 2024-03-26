b3129c8695d4d713d198850caf007a35b30dbdca
iliaman <iliaman1993@gmail.com>
2024-03-26 13:00:39 +0300

    Basic Joins

---
+++ SQL 50/1934. Confirmation Rate.sql
@@ -0,0 +1,27 @@
+-- SQL Schema
+Create table If Not Exists Signups (user_id int, time_stamp datetime)
+Create table If Not Exists Confirmations (user_id int, time_stamp datetime, action ENUM('confirmed','timeout'))
+Truncate table Signups
+insert into Signups (user_id, time_stamp) values ('3', '2020-03-21 10:16:13')
+insert into Signups (user_id, time_stamp) values ('7', '2020-01-04 13:57:59')
+insert into Signups (user_id, time_stamp) values ('2', '2020-07-29 23:09:44')
+insert into Signups (user_id, time_stamp) values ('6', '2020-12-09 10:39:37')
+Truncate table Confirmations
+insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-01-06 03:30:46', 'timeout')
+insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-07-14 14:00:00', 'timeout')
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-12 11:57:29', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-13 12:58:28', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-14 13:59:27', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-01-22 00:00:00', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-02-28 23:59:59', 'timeout')
+
+-- Write your PostgreSQL query statement below
+select user_id, coalesce(round(avg(case when action = 'confirmed' then 1 else 0 end), 2), 0) as confirmation_rate
+from Confirmations right join Signups using(user_id)
+group by user_id;
+
+-- Good solution
+SELECT s.user_id, ROUND(COALESCE(AVG((c.action='confirmed')::INT), 0), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c USING (user_id)
+GROUP BY s.user_id;
