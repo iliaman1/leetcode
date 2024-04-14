d221f6541f19b9bbced3811c042c3e6d2f858f2a
iliaman <iliaman1993@gmail.com>
2024-04-14 18:43:58 +0300

    DISTINCT

---
+++ SQL 50/5. Advanced Select and Joins/180. Consecutive Numbers.sql
@@ -0,0 +1,19 @@
+-- SQL Schema
+Create table If Not Exists Logs (id int, num int)
+Truncate table Logs
+insert into Logs (id, num) values ('1', '1')
+insert into Logs (id, num) values ('2', '1')
+insert into Logs (id, num) values ('3', '1')
+insert into Logs (id, num) values ('4', '2')
+insert into Logs (id, num) values ('5', '1')
+insert into Logs (id, num) values ('6', '2')
+insert into Logs (id, num) values ('7', '2')
+
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1, Logs l2, Logs l3
+where l1.id = l2.id - 1
+    AND l2.id = l3.id - 1
+    AND l1.num = l2.num
+    AND l2.num = l3.num
+;
