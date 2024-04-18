850c98eed51436e018e1382f582657569ce3ab26
iliaman <iliaman1993@gmail.com>
2024-04-18 18:26:14 +0300

    Array, Depth-First Search, Breadth-First Search, Matrix

---
+++ problems/easy/463. Island Perimeter.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def island_perimeter(grid: List[List[int]]) -> int:
+        perimeter = 0
+        rows, cols = len(grid), len(grid[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += 4
+                    if r > 0 and grid[r - 1][c] == 1:
+                        perimeter -= 2
+                    if c > 0 and grid[r][c - 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
+
+
+class RecursiveSolution:
+    @staticmethod
+    def island_perimeter(grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        perimeter = 0
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
+                return 1
+            if grid[r][c] == -1:
+                return 0
+            grid[r][c] = -1
+            return (dfs(r + 1, c) +
+                    dfs(r - 1, c) +
+                    dfs(r, c + 1) +
+                    dfs(r, c - 1))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += dfs(r, c)
+
+        return perimeter
