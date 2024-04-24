c88d2b4908d961f0774f0615767b991b228d6a19
iliaman <iliaman1993@gmail.com>
2024-04-24 18:20:52 +0300

    WITH, JOIN

---
+++ SQL 50/6. Subqueries/185. Department Top Three Salaries.sql
@@ -0,0 +1,38 @@
+-- SQL Schema
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int)
+Create table If Not Exists Department (id int, name varchar(255))
+Truncate table Employee
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '85000', '1')
+insert into Employee (id, name, salary, departmentId) values ('2', 'Henry', '80000', '2')
+insert into Employee (id, name, salary, departmentId) values ('3', 'Sam', '60000', '2')
+insert into Employee (id, name, salary, departmentId) values ('4', 'Max', '90000', '1')
+insert into Employee (id, name, salary, departmentId) values ('5', 'Janet', '69000', '1')
+insert into Employee (id, name, salary, departmentId) values ('6', 'Randy', '85000', '1')
+insert into Employee (id, name, salary, departmentId) values ('7', 'Will', '70000', '1')
+Truncate table Department
+insert into Department (id, name) values ('1', 'IT')
+insert into Department (id, name) values ('2', 'Sales')
+
+-- Write your PostgreSQL query statement below
+WITH e AS (
+    SELECT
+        name,
+        departmentId,
+        salary,
+        DENSE_RANK() over(
+            PARTITION by departmentId
+            ORDER BY
+                salary DESC
+        ) AS rank
+    FROM
+        Employee
+)
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary
+FROM
+    e
+    JOIN Department AS d ON e.departmentId = d.id
+WHERE
+    e.rank <= 3;
