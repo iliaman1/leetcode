33149fadcbd736320726e0480a98a17410e99f3e
iliaman <iliaman1993@gmail.com>
2024-05-10 16:23:01 +0300

    Array, Two Pointers, Binary Search, Sorting, Heap (Priority Queue)

---
+++ problems/medium/786. K-th Smallest Prime Fraction.py
@@ -0,0 +1,31 @@
+class Solution:
+    @staticmethod
+    def kth_smallest_prime_fraction(arr: list[int], k: int) -> list[int]:
+        n = len(arr)
+        left, right = 0, 1
+        res = []
+
+        while left <= right:
+            mid = left + (right - left) / 2
+            j, total, num, den = 1, 0, 0, 0
+            maxFrac = 0
+            for i in range(n):
+                while j < n and arr[i] >= arr[j] * mid:
+                    j += 1
+
+                total += n - j
+
+                if j < n and maxFrac < arr[i] * 1.0 / arr[j]:
+                    maxFrac = arr[i] * 1.0 / arr[j]
+                    num, den = i, j
+
+            if total == k:
+                res = [arr[num], arr[den]]
+                break
+
+            if total > k:
+                right = mid
+            else:
+                left = mid
+
+        return res
