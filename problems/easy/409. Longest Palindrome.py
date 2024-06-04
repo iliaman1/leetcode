6d239a1e2eb85bc85e13e8f6c9dfa952ed2dede9
iliaman <iliaman1993@gmail.com>
2024-06-04 17:47:59 +0300

    Hash Table, String, Greedy

---
+++ problems/easy/409. Longest Palindrome.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution:
+    @staticmethod
+    def longest_palindrome(s: str) -> int:
+        char_set = set()
+        length = 0
+
+        for char in s:
+            if char in char_set:
+                char_set.remove(char)
+                length += 2
+            else:
+                char_set.add(char)
+
+        if char_set:
+            length += 1
+
+        return length
